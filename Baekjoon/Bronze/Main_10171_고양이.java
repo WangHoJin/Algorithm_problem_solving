233f6933326b34b4b0e17bf4bc18432be3a88ed2
왕호진 <kkgmco0825@gmail.com>
2021-08-01 20:06:59 +0900

    백준 등급별 폴더 생성

---
+++ Baekjoon/Bronze/Main_10171_고양이.java
@@ -0,0 +1,16 @@
+package Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_10171_고양이 {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
---
+++ Baekjoon/Bronze/Main_10172_개.java
@@ -0,0 +1,17 @@
+package Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_10172_개 {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+}
