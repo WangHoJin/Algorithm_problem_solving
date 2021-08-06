0ddeb813e774fb00f75fdc28b483898ab4ab7f1c
왕호진 <kkgmco0825@gmail.com>
2021-08-06 23:31:08 +0900

    BOJ10950_A+B-3 시리즈

--- Baekjoon/Bronze/Main_10951_AplusBminus4.java
+++ Baekjoon/Bronze/Main_10951_AplusBminus4.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_10950_AplusBminus4 {
+public class Main_10951_AplusBminus4 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
--- Baekjoon/Bronze/Main_10952_AplusBminus5.java
+++ Baekjoon/Bronze/Main_10952_AplusBminus5.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_10950_AplusBminus5 {
+public class Main_10952_AplusBminus5 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
